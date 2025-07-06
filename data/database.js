0b75e43dce3b16f9771b72b8a76426091c770fd7
echoedinvoker <echoedinvoker@gmail.com>
2025-07-06 07:29:04 +0000

    default env values

--- data/database.js
+++ data/database.js
@@ -1,18 +1,19 @@
 import { MongoClient } from 'mongodb';
 
-const connectionProtocol = process.env.MONGODB_CONNECTION_PROTOCOL;
-const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS;
-const dbUser = process.env.MONGODB_USERNAME;
-const dbPassword = process.env.MONGODB_PASSWORD;
-const dbName = process.env.MONGODB_DB_NAME;
+const connectionProtocol = process.env.MONGODB_CONNECTION_PROTOCOL || 'mongodb+srv';
+const clusterAddress = process.env.MONGODB_CLUSTER_ADDRESS || 'cluster0.tefzwkz.mongodb.net';
+const dbUser = process.env.MONGODB_USERNAME || 'echoedinvoker';
+const dbPassword = process.env.MONGODB_PASSWORD || 'CfQoBFGjjEuY67MD';
+const dbName = process.env.MONGODB_DB_NAME; 'gha-demo';
 
 const uri = `${connectionProtocol}://${dbUser}:${dbPassword}@${clusterAddress}/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log('MongoDB URI:', uri);
 const client = new MongoClient(uri);
 
 console.log('Trying to connect to db');
 
 try {
-  console.log('Connected to MongoDB server...', client);
+  console.log('Connected to MongoDB server...');
   await client.connect();
   console.log('Connected to MongoDB server successfully');
   await client.db(dbName).command({ ping: 1 });
